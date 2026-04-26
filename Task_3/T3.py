bc7bfad7298b3ebb715f405842266cb953f31368
Piyush Pandey <thepiyushpandey75@gmail.com>
2026-04-26 12:11:16 +0530

    Format small_letters and clean up code

--- Task_3/T3.py
+++ Task_3/T3.py
@@ -1,7 +1,7 @@
 import random
-small_letters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+small_letters=['a', 'b', 'c',' d', 'e','f', 'g', 'h',' i', 'j', 'k', 'l',' m', 'n',' o', 'p', 'q', 'r','s', 't','u', 'v','w','x',' y', 'z']
 capital_letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-numbers=['1','2','3','4','5','6','7','8','9']
+numbers=['1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols=['@','#','$','%','&','*']
 
 print("welcome to random password generator")
@@ -19,10 +19,8 @@ for char in range(0,nr_number):
     password_list.append(random.choice(numbers))
 for char in range(0,nr_symbol):
     password_list.append(random.choice(symbols))
-#print(password_list)
-#shuffle works only with list not string
+    
 random.shuffle(password_list)
-#print(password_list)
 
 password=""
 for char in password_list:
